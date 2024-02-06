5593ec59d871113ccabdf0a4cda9c645d8f2b277
elver <joelros2003@gmail.com>
2024-02-06 13:16:03 +0100

    working one

--- src/commands.rs
+++ src/commands.rs
@@ -1,16 +1,20 @@
 use std::fmt;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Commands {
     NOP,
-    ADD,
-    JNE
+    ADD(usize),
+    JNZ(usize),
+    FUN
 }
 
 #[derive(Debug, Clone)]
 pub enum Errors {
     FunctionNotThere,
-    InvalidToken(String)
+    InvalidContinuation(Commands,String),
+    InvalidToken(String),
+    FunctionAlreadyInUse(String),
+    FunctionDoesntExist(String)
 }
 
 
@@ -20,12 +24,26 @@ pub struct TokenError{
     pub line: (usize,usize)
 }
 
+impl fmt::Display for Commands {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Commands::ADD(a) => write!(f,"ADD {}",a),
+            Commands::JNZ(a) => write!(f,"JNZ {}",a),
+            Commands::FUN |
+            Commands::NOP=> todo!()
+        }
+    }
+}
+
 
 impl fmt::Display for TokenError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self.error_type {
             Errors::FunctionNotThere => write!(f, "no function found to call in {:?}", self.line),
-            Errors::InvalidToken(token) => write!(f, "token {} not found {:?}", token ,self.line)
+            Errors::InvalidContinuation(command,token) => write!(f, "found {} after <{}>, this is the wrong type {:?}", token, command,self.line),
+            Errors::InvalidToken(token) => write!(f, "token <{}> not found {:?}", token ,self.line),
+            Errors::FunctionAlreadyInUse(fun) => write!(f, "function <{}> already exists {:?}", fun ,self.line),
+            Errors::FunctionDoesntExist(fun) => write!(f, "function <{}> doesn't exist or is uninitialized (use in line {})", fun ,self.line.0),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
+use std::iter::zip;
 use std::time::Instant;
 
 mod commands;
@@ -28,23 +28,24 @@ fn main() {
         }
     };
     if VERBOSE {
-        println!("     {ANSI_BOLD}{ANSI_GREEN}Reading{ANSI_RESET} file in {:.2}s", start.elapsed().as_millis() as f32/1000.0);
+        println!("     {ANSI_BOLD}{ANSI_GREEN}Reading{ANSI_RESET} file in {:.2}s", timer.elapsed().as_millis() as f32/1000.0);
         timer = Instant::now();
     }
     let separated: Vec<Vec<String>> = separator(lines);
-    let (tokenized, function_headers) = match new_func(separated) {
-        Ok((s,v)) => (s,v),
+    let tokenized = match new_func(separated) {
+        Ok(s) => s,
         Err(e) => {
             println!("{ANSI_BOLD}{ANSI_RED}Failed to parse input{ANSI_RESET}: {}", e);
             ::std::process::exit(2);
         }
     }; 
     if VERBOSE {
-        println!("     {ANSI_BOLD}{ANSI_GREEN}Reading{ANSI_RESET} file in {:.2}s", timer.elapsed().as_millis() as f32/1000.0);
-        timer = Instant::now();
+        println!("     {ANSI_BOLD}{ANSI_GREEN}Parsing{ANSI_RESET} file in {:.2}s", timer.elapsed().as_millis() as f32/1000.0);
+        //timer = Instant::now();
     }
-    dbg!(tokenized);
-    dbg!(function_headers);
+    println!("{ANSI_BOLD}{ANSI_GREEN}Compilation complete{ANSI_RESET}: file in {:.2}s", start.elapsed().as_millis() as f32/1000.0);
+
+    println!("{:?}",tokenized);
 
 }
 
@@ -68,14 +69,16 @@ fn separator(input: Vec<String>) -> Vec<Vec<String>> {
         .collect()
 }
 
-fn new_func(input: Vec<Vec<String>>) -> Result<(Vec<Commands>, HashMap<String,usize>),TokenError>{
+fn new_func(input: Vec<Vec<String>>) -> Result<Vec<Commands>,TokenError>{
     const FUNCTION_INTRO:char = '.';
-    let mut fnpointer: usize = 0;
-    let mut functions: HashMap<String, usize> = HashMap::new();
+    let mut fncounter = 0;
     let mut next_func: bool = false;
     let mut l:usize=0;
     let mut token: Commands = Commands::NOP;
-    let mut tokens: Vec<(Commands,u8)> = Vec::new();
+    let mut tokens: Vec<Commands> = Vec::new();
+    let mut funjumps: Vec<String> = Vec::new();
+    let mut functions: Vec<(String, usize)> = Vec::new();
+    let mut calls: Vec<usize> = Vec::new();
     for (i, line) in input.iter().enumerate() {
         //i+1
         if next_func {
@@ -86,51 +89,89 @@ fn new_func(input: Vec<Vec<String>>) -> Result<(Vec<Commands>, HashMap<String,us
         }
         let mut counter: usize = 0;
         for word in line {
-            if next_func {
-                if word == "" {
-                    return Err(TokenError { error_type: Errors::FunctionNotThere, line: (i+1,counter)});
-                }
-                else {
-                    functions.insert(word.to_string(), fnpointer);
-                    next_func=false;
-                    continue;
-                }
-            }
             if word == "" {
                 counter+=1;
                 continue;
             } 
-            if token != Commands::NOP {
-                if word.len()>2 {
-                    let num = match word[2] {
-                        "x" => {word[2..].parse()}
+            if next_func {
+                if word == "" {
+                    return Err(TokenError { error_type: Errors::FunctionNotThere, line: (i+1,counter)});
+                } else {
+                    token = Commands::FUN;
+                    
+                    if functions.iter().any(|(s,_)| s == &word[1..]) {
+                        return Err(TokenError { error_type: Errors::FunctionAlreadyInUse(word[1..].to_string()), line: (i+1,counter) })
                     }
-                }
-                match token {
-                    Commands::ADD => {
-                        
-                    }
-                    Commands::NOP|
-                    Commands::JNE => ()
+                    functions.push((word.to_string(), fncounter));
+                    next_func=false;
                 }
             }
-            if word.starts_with(FUNCTION_INTRO) {
+            if token != Commands::NOP {
+                //if word.len()>2 {
+                //    let num: Result<T, _> = match word[2] {
+                //        "x" => word[2..].parse()
+                //    }
+                //todo parse other base like 0b 0x and 0o
+                //}
+                match token {
+                    Commands::ADD(_) => {
+                        match word.parse::<usize>().ok() {
+                            Some(n) => tokens.push(Commands::ADD(n)),
+                            None => return Err(TokenError { error_type: Errors::InvalidContinuation(token, word.to_owned()), line: (i+1,counter)}),
+                        }
+                    },
+                    Commands::FUN => {
+                        tokens.push(token);
+                    },
+                    Commands::JNZ(_) => {
+                        calls.push(fncounter);
+                        funjumps.push(word.to_string());
+                        tokens.push(Commands::JNZ(funjumps.len()-1))
+                    }
+                    Commands::NOP => ()
+                }
+                token = Commands::NOP;
+                fncounter+=1;
+            } 
+            else if word.starts_with(FUNCTION_INTRO) {
                 if word[1..] == "".to_owned() {
                     next_func = true;
                     continue;
                 }
-                functions.insert(word[1..].to_string(), fnpointer);
-                next_func=false;
+                if functions.iter().any(|(s,_)| s == &word[1..]) {
+                    return Err(TokenError { error_type: Errors::FunctionAlreadyInUse(word[1..].to_string()), line: (i+1,counter) })
+                }
+                functions.push((word[1..].to_string(), functions.len()));
+                next_func = false;
+                tokens.push(Commands::FUN);
+                fncounter +=1;
+            } 
+            else {
+                token = match word.to_ascii_uppercase().as_str() {
+                    "ADD" => Commands::ADD(0),
+                    "JNZ" => Commands::JNZ(0),
+                    _ => return Err(TokenError { error_type: Errors::InvalidToken(word.to_owned()), line: (i+1,counter) })
+                };
             }
-            match word.to_ascii_uppercase().as_str() {
-                "ADD" => token = Commands::ADD,
-                _ => ()
-            }
+            
+            counter += word.len()+1;
         }
         l=i;
     }
     if next_func {
         return Err(TokenError { error_type: Errors::FunctionNotThere, line: (l+2,0)});
     }
-    Ok((vec![],functions))
+
+    for (i,s)  in zip(calls, funjumps) {
+        match functions.iter().filter(|(a,_)| a==&s).collect::<Vec<&(String,usize)>>().pop() {
+            Some((_,y)) => {
+                tokens[i] = match tokens[i] {
+                    Commands::JNZ(_) => Commands::JNZ(*y),
+                    _ => panic!("wtf? fixing error?")
+                }
+            }
+            None => return Err(TokenError { error_type: Errors::FunctionDoesntExist(s), line: (i,0) })
+        };
+    }
+    Ok(tokens)
 }
